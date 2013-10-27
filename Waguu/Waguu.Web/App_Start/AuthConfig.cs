e49b10c111ae954fba5ed572a810996adbad882e
booyuan <booyuan@gmail.com>
2013-10-27 05:01:59 -0700

    Enable external authentication
    facebook, twitter, google and microsoft

--- Waguu/Waguu.Web/App_Start/AuthConfig.cs
+++ Waguu/Waguu.Web/App_Start/AuthConfig.cs
@@ -14,19 +14,19 @@ namespace Waguu.Web
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
 
-            //OAuthWebSecurity.RegisterMicrosoftClient(
-            //    clientId: "",
-            //    clientSecret: "");
+            OAuthWebSecurity.RegisterMicrosoftClient(
+                clientId: "0000000044107109",
+                clientSecret: "Q-mwGmncotnOTnwtGLlPWafuIDRtJi4j");
 
-            //OAuthWebSecurity.RegisterTwitterClient(
-            //    consumerKey: "",
-            //    consumerSecret: "");
+            OAuthWebSecurity.RegisterTwitterClient(
+                consumerKey: "Dw93e3uaohNppRCe9kSvw",
+                consumerSecret: "D6tmtHJZ3XdT3NJdMmzBFY9IWc49hBgjyu3ArBcnM");
 
-            //OAuthWebSecurity.RegisterFacebookClient(
-            //    appId: "",
-            //    appSecret: "");
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "648054308550797",
+                appSecret: "1aa39c980c70acdd7035fd8185654aef");
 
-            //OAuthWebSecurity.RegisterGoogleClient();
+            OAuthWebSecurity.RegisterGoogleClient();
         }
     }
 }
